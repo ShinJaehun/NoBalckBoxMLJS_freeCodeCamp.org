8c1b5010fdcf3b6c6f1d7350aacc541f693d6a62
ShinJaehun <gunsmoke.shin@gmail.com>
2023-05-02 15:49:22 +0900

    Custom Chart

--- common/utils.js
+++ common/utils.js
@@ -2,16 +2,29 @@ const utils = {};
 
 utils.flaggedUsers=[];
 
+// for google chart
+// utils.styles={
+//     car:'gray',
+//     fish: 'red',
+//     house: 'yellow',
+//     tree: 'green',
+//     bicycle: 'cyan',
+//     guitar: 'blue',
+//     pencil: 'magenta',
+//     clock: 'lightgray',
+// };
+
+// for custom chart
 utils.styles={
-    car:'gray',
-    fish: 'red',
-    house: 'yellow',
-    tree: 'green',
-    bicycle: 'cyan',
-    guitar: 'blue',
-    pencil: 'magenta',
-    clock: 'lightgray',
-}
+    car: { color: 'gray', text: '🚗' },
+    fish: { color: 'red', text: '🐟' },
+    house: { color: 'yellow', text: '🏠' },
+    tree: { color: 'green', text: '🌳' },
+    bicycle: { color: 'cyan', text: '🚲' },
+    guitar: { color: 'blue', text: '🎸' },
+    pencil: { color: 'magenta', text: '✏️' },
+    clock: { color: 'lightgray', text: '⏰' },
+};
 
 utils.formatPercent = (n) => {
     return (n*100).toFixed(2)+"%";
